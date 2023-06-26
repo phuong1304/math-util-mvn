3fc3d9dc6d327659bf2a71f340886969e96d8779
bichphuong <bichphuong13042002@gmail.com>
2023-06-26 16:50:57 +0700

    update code to use recursion and see green forever

--- src/main/java/org/bipu/mathutil/core/MathUtility.java
+++ src/main/java/org/bipu/mathutil/core/MathUtility.java
@@ -21,18 +21,34 @@ public class MathUtility {
     // hàm chỉ cho phép n = 0..20
     // đưa n > 20: NÉM NGOẠI LỆ
     
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
+//                    
+//        if(n == 0 || n == 1)
+//            return 1;
+//        
+//        long product = 1; // biến gom tích nhân với nhau
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        
+//        return product;
+//    }
+    
+      public static long getFactorial(int n){
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0..20, plz");
                     
         if(n == 0 || n == 1)
             return 1;
         
-        long product = 1; // biến gom tích nhân với nhau
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        
-        return product;
-    }
+      return n * getFactorial(n - 1);
+    }// đệ quy - recursion
     
 }
+//5!=1.2.3.4.5=4!x 5= 5x4!
+// 4!=4x 3!
+//3!= 3 x 2!
+//2! = 2x1!
+//1! = 1; //DỪng
+//N! = N x (N-1)!
--- src/test/java/org/bipu/mathutil/core/test/MathUtilityAdvancedTest.java
+++ src/test/java/org/bipu/mathutil/core/test/MathUtilityAdvancedTest.java
@@ -29,7 +29,7 @@ public class MathUtilityAdvancedTest {
         {1, 1},
         {2, 2},
         {4, 24},
-        {5, 1200}};
+        {5, 120}};
 
         return testData;
     }
